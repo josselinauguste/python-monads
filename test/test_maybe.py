2785f6c613b911be68424edc97facf245ab07fff
Josselin Auguste <jauguste@iblop.net>
2019-11-24 14:36:25 +0100

    Add monadic laws test file

---
+++ test/test_maybe.py
@@ -0,0 +1,18 @@
+def test_functor_identity():
+    assert False
+
+
+def test_functor_composition():
+    assert False
+
+
+def test_monad_left_identity():
+    assert False
+
+
+def test_monad_right_identity():
+    assert False
+
+
+def test_monad_associativity():
+    assert False
